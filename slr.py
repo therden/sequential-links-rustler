113d2f935a0f97def1c1e581bb6ea0025009d98a
Thomas Herden <tomherden@gmail.com>
2021-08-18 09:48:43 -0400

    Save Options and window locationto slr.json file in application directory upon Exit, and read upon Startup.

--- slr.py
+++ slr.py
@@ -27,6 +27,7 @@ rustler_logo = sg.Image(
     enable_events=False,
 )
 window_title = "Sequential Links Rustler"
+sg.user_settings_filename(path=".")
 
 # create Fixed gui elements
 button_rustle = sg.Button(
@@ -35,7 +36,7 @@ button_rustle = sg.Button(
     font=("Any", 12),
     bind_return_key=True,
     key="-DoIt-",
-    focus=True
+    focus=True,
 )
 button_options = sg.Button(
     "Show Options", size=(16, 2), font=("Any", 12), key="-Options-"
@@ -45,8 +46,12 @@ label_URLmask = sg.Text(
     " Enter or Edit URL mask below", text_color="black", font=("Any", 10)
 )
 input_URLmask = sg.Multiline(key="-URLMask-", size=(62, 4))
-button_automask = sg.Button("Convert URL", size=(14, 1), key="-Insert range defs-", pad=(0, 0))
-button_clear = sg.Button("Clear URL mask", size=(14, 1), key="-Clear URL mask-", pad=(0, 0))
+button_automask = sg.Button(
+    "Convert URL", size=(14, 1), key="-Insert range defs-", pad=(0, 0)
+)
+button_clear = sg.Button(
+    "Clear URL mask", size=(14, 1), key="-Clear URL mask-", pad=(0, 0)
+)
 
 # create Options gui elements
 button_reset = sg.Button("Restore default filepath and name", key="-HTML_Defaults-")
@@ -56,27 +61,41 @@ label_file_options = sg.Text(
 )
 spin_thumbsize = sg.Spin(
     [str(each) for each in range(1, 101)],
-    initial_value="13",
+    initial_value=sg.user_settings_get_entry(key="-ThumbSize-", default="13"),
     size=(4, 4),
     key="-ThumbSize-",
 )
 label_percent_width = sg.Text("(% of browser window width)")
 input_hideborked = sg.Checkbox(
-    " Hide broken image links", default=False, key="-HideBorkedImages-"
+    " Hide broken image links",
+    default=sg.user_settings_get_entry(key="-HideBorkedImages-", default=False),
+    key="-HideBorkedImages-",
 )
 label_filepath = sg.Text("Path:")
-input_filepath = sg.Input(key="-FilePath-", size=(31, 1), default_text=home_dir)
+input_filepath = sg.Input(
+    key="-FilePath-",
+    size=(31, 1),
+    default_text=sg.user_settings_get_entry(key="-FilePath-", default=home_dir),
+)
 label_filename = sg.Text("Name:")
-input_filename = sg.Input(key="-FileName-", size=(15, 1), default_text="rustled.html")
+input_filename = sg.Input(
+    key="-FileName-",
+    size=(15, 1),
+    default_text=sg.user_settings_get_entry(key="-FileName-", default="rustled.html"),
+)
 input_delete = sg.Checkbox(
-    " Delete HTML file on Exit", default=True, key="-DeleteFile-"
+    " Delete HTML file on Exit",
+    default=sg.user_settings_get_entry(key="-DeleteFileOnExit-", default=True),
+    key="-DeleteFileOnExit-",
 )
 label_browser = sg.Text(
     "Choose browser ", size=(18, 2), text_color="black", justification="right"
 )
 input_browser = sg.Combo(
     supported_browsers,
-    default_value="system_default",
+    default_value=sg.user_settings_get_entry(
+        key="-SelectedBrowser-", default="system_default"
+    ),
     size=(20, 1),
     pad=(1, 1),
     readonly=True,
@@ -99,11 +118,7 @@ fixed_column_1 = sg.Column(
 fixed_column_2 = sg.Column(
     [
         [sg.Text(" ")],
-        [
-            button_rustle,
-            sg.vbottom(button_options),
-            sg.vbottom(button_exit)
-        ],
+        [button_rustle, sg.vbottom(button_options), sg.vbottom(button_exit)],
         # [sg.Text(" ")],
         [label_URLmask],
         [input_URLmask],
@@ -127,25 +142,14 @@ options_column = sg.Column(
             sg.Frame(
                 "HTML File Options",
                 [
-                    [
-                        label_filepath,
-                        input_filepath,
-                        label_filename,
-                        input_filename,
-                    ],
+                    [label_filepath, input_filepath, label_filename, input_filename,],
                     [sg.Text("", size=(4, 1)), button_reset],
                     [input_delete],
                 ],
                 title_color="black",
             )
         ],
-        [
-            sg.Frame(
-                "Choose Browser",
-                [[input_browser]],
-                title_color="black",
-            )
-        ],
+        [sg.Frame("Choose Browser", [[input_browser]], title_color="black",)],
     ],
     key="options",
 )
@@ -154,17 +158,26 @@ column_a = sg.Column([[fixed_column_1]])
 column_b = sg.Column(
     [
         [fixed_column_2],
-        [sg.pin(options_column, shrink=True, expand_x=True, expand_y=True)]
+        [sg.pin(options_column, shrink=True, expand_x=True, expand_y=True)],
     ]
 )
 layout = [[sg.vtop(column_a), column_b]]
 
 # Create the window
-window = sg.Window(window_title, layout, font=("Any", 10), icon=icon_file, finalize=True)
+# window = sg.Window(window_title, layout, font=("Any", 10), icon=icon_file, finalize=True)
+window = sg.Window(
+    window_title,
+    layout,
+    font=("Any", 10),
+    icon=icon_file,
+    # enable_close_attempted_event=True,
+    location=sg.user_settings_get_entry("-position-", (None, None)),
+    finalize=True,
+)
 
 
 def toggle_option_elements():
-    window["options"].update(visible = not window["options"].visible)
+    window["options"].update(visible=not window["options"].visible)
 
 
 toggle_option_elements()
@@ -173,8 +186,19 @@ window["-DoIt-"].set_focus()
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == "Exit":
+        sg.user_settings_set_entry("-ThumbSize-", values["-ThumbSize-"])
+        sg.user_settings_set_entry("-HideBorkedImages-", values["-HideBorkedImages-"])
+        sg.user_settings_set_entry("-SelectedBrowser-", values["-SelectedBrowser-"])
+        sg.user_settings_set_entry("-FilePath-", values["-FilePath-"])
+        sg.user_settings_set_entry("-FileName-", values["-FileName-"])
+        sg.user_settings_set_entry("-DeleteFileOnExit-", values["-DeleteFileOnExit-"])
+        location = window.current_location()
+        location = (location[0], location[1] - 23)
+        # adjustment needed -- at least on linux w/PySimpleGUI "4.46.0.5 Unreleased"
+        sg.user_settings_set_entry("-position-", location)
+
         try:
-            if values["-DeleteFile-"]:
+            if values["-DeleteFileOnExit-"]:
                 try:
                     remove(values["-FilePath-"] + "/" + values["-FileName-"])
                 except Exception:
